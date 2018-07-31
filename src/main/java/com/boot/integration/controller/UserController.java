d2794c4c264f3b41b8acf8b6adec41b174874c0b
张耀 <yao.zhang@wuliangroup.com>
2018-07-31 15:42:46 +0800

    注释rabbitmq

--- src/main/java/com/boot/integration/controller/UserController.java
+++ src/main/java/com/boot/integration/controller/UserController.java
@@ -1,20 +1,13 @@
 package com.boot.integration.controller;
 
-import com.boot.integration.conf.rabbit.MessageProducer;
 import com.boot.integration.constant.ResponseDto;
-import com.boot.integration.constant.ResponseEnum;
-import com.boot.integration.mapper.UserMapper;
 import com.boot.integration.model.User;
 import com.boot.integration.service.UserService;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * Created by haoyong on 2018/1/4.
@@ -25,9 +18,6 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private MessageProducer messageProducer;
 
     /*
       token获取
@@ -40,11 +30,4 @@ public class UserController {
         return userService.queryUserRoles();
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN')")
-    @RequestMapping(value = "/test/rabbit",method = RequestMethod.GET)
-    public void testRabbit(){
-        User user = new User();
-        user.setUsername("author");
-        messageProducer.sendMessage(user);
-    }
 }
