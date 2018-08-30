999e014f4a96a74be052a3eebce55f7ad169f14b
郝勇 <yong.hao@wuliangroup.com>
2018-08-30 16:09:33 +0800

    druid监控页配置

--- src/main/java/com/boot/integration/conf/druid/DruidAutoConfiguration.java
+++ src/main/java/com/boot/integration/conf/druid/DruidAutoConfiguration.java
@@ -57,16 +57,18 @@ public class DruidAutoConfiguration
     public ServletRegistrationBean druidServlet()
     {
         logger.info("init Druid Servlet Configuration ");
+
         ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
         // IP白名单
-        servletRegistrationBean.addInitParameter("allow", "192.168.2.25,127.0.0.1");
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
         // IP黑名单(共同存在时，deny优先于allow)
-        servletRegistrationBean.addInitParameter("deny", "192.168.1.100");
+        servletRegistrationBean.addInitParameter("deny", "192.168.7.190");
         //控制台管理用户
         servletRegistrationBean.addInitParameter("loginUsername", "admin");
         servletRegistrationBean.addInitParameter("loginPassword", "admin");
         //是否能够重置数据 禁用HTML页面上的“Reset All”功能
         servletRegistrationBean.addInitParameter("resetEnable", "false");
+
         return servletRegistrationBean;
     }
 
@@ -76,6 +78,7 @@ public class DruidAutoConfiguration
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
         filterRegistrationBean.addUrlPatterns("/*");
         filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+
         return filterRegistrationBean;
     }
 }
