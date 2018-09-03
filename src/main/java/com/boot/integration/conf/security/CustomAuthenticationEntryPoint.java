22dee0353eabb0d54a36a9135c822c8207c37e3d
郝勇 <yong.hao@wuliangroup.com>
2018-09-03 11:55:51 +0800

    注释修改

--- src/main/java/com/boot/integration/conf/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/boot/integration/conf/security/CustomAuthenticationEntryPoint.java
@@ -19,8 +19,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint
 {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
-        throws ServletException
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws ServletException
     {
         ResponseDto responseDto = new ResponseDto();
         responseDto.setRetCode(CustomCode.ERROR_TOKEN.getValue());
