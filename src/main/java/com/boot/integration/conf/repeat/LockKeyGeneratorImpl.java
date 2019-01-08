0f00ae4ec31818a8d4662911e65b41e68b5c22ec
郝勇 <yong.hao@wuliangroup.com>
2019-01-08 18:31:34 +0800

    提交

--- src/main/java/com/boot/integration/conf/repeat/LockKeyGeneratorImpl.java
+++ src/main/java/com/boot/integration/conf/repeat/LockKeyGeneratorImpl.java
@@ -27,9 +27,12 @@ public class LockKeyGeneratorImpl implements LockKeyGenerator
         MethodSignature signature = (MethodSignature)pjp.getSignature();
         Method method = signature.getMethod();
         CacheLock lockAnnotation = method.getAnnotation(CacheLock.class);
+
         final Object[] args = pjp.getArgs();
         final Parameter[] parameters = method.getParameters();
+
         StringBuilder builder = new StringBuilder();
+
         // TODO 默认解析方法里面带 CacheParam 注解的属性,如果没有尝试着解析实体对象中的
         for (int i = 0; i < parameters.length; i++)
         {
