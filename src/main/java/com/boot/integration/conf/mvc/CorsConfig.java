7a4b85fe4053f636df44d383cdfdf8b7bec1815f
郝勇 <yong.hao@wuliangroup.com>
2019-07-30 14:16:16 +0800

    更改cors

--- src/main/java/com/boot/integration/conf/mvc/CorsConfig.java
+++ src/main/java/com/boot/integration/conf/mvc/CorsConfig.java
@@ -13,9 +13,13 @@ public class CorsConfig
     private CorsConfiguration buildConfig()
     {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*"); // 1
-        corsConfiguration.addAllowedHeader("*"); // 2
-        corsConfiguration.addAllowedMethod("*"); // 3
+        // 1. 允许所有的请求域名访问，可以固定单条或者多条内容，如："http://www.baidu.com"，只有百度可以访问我们的跨域资源。
+        corsConfiguration.addAllowedOrigin("*");
+        // 2. 允许所有的请求头访问，可以自定义设置任意请求头信息，如："X-YAUTH-TOKEN"。
+        corsConfiguration.addAllowedHeader("*");
+        // 3. 允许所有的请求方法访问，如：POST、GET、PUT、DELETE等。
+        corsConfiguration.addAllowedMethod("*");
+
         return corsConfiguration;
     }
 
@@ -23,7 +27,9 @@ public class CorsConfig
     public CorsFilter corsFilter()
     {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        // 4. 配置可以被跨域的路径，可以任意配置，可以具体到直接请求路径。
+        source.registerCorsConfiguration("/**", buildConfig());
+
         return new CorsFilter(source);
     }
 }
