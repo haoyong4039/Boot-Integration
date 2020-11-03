eab648eedfd55f0fc9b550ae01cf5425d7f47d78
haoy <1241907185@qq.com>
2020-11-03 10:45:42 +0800

    删除多余注解

--- src/main/java/com/boot/integration/IntegrationApplication.java
+++ src/main/java/com/boot/integration/IntegrationApplication.java
@@ -2,14 +2,12 @@ package com.boot.integration;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @EnableWebMvc
 @EnableWebSocket
-@EnableAutoConfiguration
 @MapperScan(basePackages = "com.boot.integration.mapper")
 @SpringBootApplication
 public class IntegrationApplication
